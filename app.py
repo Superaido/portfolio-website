ddec1d9e49ac564be2447fa1d1a9ee8dfecb0ff3
Adrianne Odette <odetteclan1@gmail.com>
2026-03-20 15:05:30 -0400

    added a unity platformer build, served at endpoint /game

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route("/game")
+def game():
+    return render_template("game.html")
 
 @app.route("/")
-def hello():
-    return render_template('index.html')
+def index():
+    return render_template("index.html")
